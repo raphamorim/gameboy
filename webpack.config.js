bbeb65ed8cbfd83635cf2dee43c8ba4ef51cdd30
Raphael Amorim <rapha850@gmail.com>
2022-10-15 14:18:49 +0200

    move to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'site'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'index.js'
     },
     plugins: [
