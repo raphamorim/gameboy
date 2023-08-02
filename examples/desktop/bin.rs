300dcbf099ffa1d493496b28abfebaebe57e16ad
Raphael Amorim <rapha850@gmail.com>
2023-08-03 00:57:47 +0200

    update example

--- examples/desktop/bin.rs
+++ examples/desktop/bin.rs
@@ -4,21 +4,13 @@ use lr35902::gameboy::{load_rom, Gameboy, RenderMode::Desktop};
 
 fn main() {
     // let gb = Gameboy::new("./../../tests/cpu_instrs/cpu_instrs.gb");
-    if let Ok((data, filepath)) = load_rom("./../pokemon-blue.gb") {
-        // let gb = Gameboy::new(data, Some(filepath));
-        let gb = Gameboy::new(data, None);
+    // if let Ok((data, filepath)) = load_rom("./../pokemon-blue.gb") {
+    if let Ok((data, filepath)) = load_rom("./../../tests/dmg_sound/dmg_sound.gb") {
+        // na web:
+        // let gb = Gameboy::new(data, None);
+        let gb = Gameboy::new(data, Some(filepath));
         gb.render(Desktop);
     } else {
         println!("error loading rom");
     }
-    
-    // match gb.load_rom("./pokemon-blue.gb") {
-    // match gb.load_rom("./sample-rom.gb") {
-    // match gb.load_rom("./../../tests/cpu_instrs/cpu_instrs.gb") {
-    //     Ok(..) => {
-    //     }
-    //     Err(err) => {
-    //         println!("{:?}", err);
-    //     }
-    // }
 }
