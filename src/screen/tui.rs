3aa634d81d92d4469028435fb1af09391ea5ceea
Raphael Amorim <rapha850@gmail.com>
2025-01-02 22:23:45 -0300

    back to elapsed

--- src/screen/tui.rs
+++ src/screen/tui.rs
@@ -94,7 +94,10 @@ fn run_app<B: Backend>(
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
 
-        let timeout = Duration::from_millis(5);
+        let timeout = app
+            .tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
         if ratatui::crossterm::event::poll(timeout)? {
             if let Event::Key(key) = event::read()? {
                 let is_pressed = key.kind == KeyEventKind::Press;
